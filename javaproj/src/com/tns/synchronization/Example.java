d13660434a5ab228ef73fe6978b005b51d15249a
CSELAB6 <CSELAB6@nani>
2025-02-12 11:44:07 +0530

    demo

---
+++ javaproj/src/com/tns/synchronization/Example.java
@@ -0,0 +1,18 @@
+package com.tns.synchronization;
+
+public class Example {
+	synchronized void display() {
+		Thread g= Thread.currentThread();
+		
+		for(int i=0;i<=3;i++) {
+			try {
+				Thread.sleep(2000);
+				System.out.println(g.getName()+" ");
+			}
+			catch (Exception e) {
+				
+			}
+		}
+	}
+
+}
---
+++ javaproj/src/com/tns/synchronization/T.java
@@ -0,0 +1,13 @@
+package com.tns.synchronization;
+
+public class T extends Thread{
+	Example e ;
+	
+	T(Example e){
+		this.e=e;
+	}
+	public void run() {
+		e.display();
+	}
+
+}
